d0a2bf17badf9421e3f470637b7dad8c1444e1f9
EngrEOOnoja <onojaemmanuel193@gmail.com>
2025-08-18 22:58:14 +0100

    Update Assignment

--- Java/WorldPopulationGrowth.java
+++ Java/WorldPopulationGrowth.java
@@ -1,7 +1,9 @@
 public class WorldPopulationGrowth {
     public static void main(String[] args) {
+       
         double currentPopulation = 7.9;
         double growthRate = 0.009;
+      
         int yearExpected = 75;
         
         
@@ -11,9 +13,11 @@ public class WorldPopulationGrowth {
            for (int year = 1; year <= yearExpected ; year++) {
             double increase = population * growthRate;
             population += increase;
+           
             if (doublePopulationYear == 0 && population >= 2 * currentPopulation) {
                 doublePopulationYear = year;
-System.out.print("The Population in year " + year + " is: " + population + ", Increase: " + increase);
+                
+	System.out.print("The Population in year " + year + " is: " + population + ", Increase: " + increase);
         }
 
              }
